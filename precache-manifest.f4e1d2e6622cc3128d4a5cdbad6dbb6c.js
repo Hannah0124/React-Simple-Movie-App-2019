6b04f719af2fbafc272e4f0b4896dc2541f8cbe7
Hannah0124 <hannahjoo24@gmail.com>
2019-11-15 21:07:49 -0800

    Updates

---
+++ precache-manifest.f4e1d2e6622cc3128d4a5cdbad6dbb6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d748991a680f789f95030cc1152c74e",
+    "url": "/movie-app-2019/index.html"
+  },
+  {
+    "revision": "eeff01401569d1b14d55",
+    "url": "/movie-app-2019/static/css/main.0a4df141.chunk.css"
+  },
+  {
+    "revision": "4c3f5906623920d7a688",
+    "url": "/movie-app-2019/static/js/2.9a2affb7.chunk.js"
+  },
+  {
+    "revision": "eeff01401569d1b14d55",
+    "url": "/movie-app-2019/static/js/main.faade921.chunk.js"
+  },
+  {
+    "revision": "94508947d3be0f17c140",
+    "url": "/movie-app-2019/static/js/runtime-main.41f814a9.js"
+  }
+]);
